5f4b7d22c2dfcd23a2d810ae668c8d63de29a282
Dubjay18 <jejeniyi7@gmail.com>
2023-12-11 09:28:20 +0100

    feat: initial db models: answer model

---
+++ src/models/answer.model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import {
+  strAnswerModelConstant,
+  strQuestionModelConstant,
+} from "./model.constants";
+
+const AnswerSchema = new mongoose.Schema({
+  question: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: strQuestionModelConstant,
+    required: [true, "Answer must belong to a question"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default mongoose.model(
+  strAnswerModelConstant,
+  AnswerSchema
+);
--- src/models/model.constants.ts
+++ src/models/model.constants.ts
@@ -2,3 +2,4 @@ export const strUserModelConstant = "User";
 export const strQuizModelConstant = "Quiz";
 export const strQuestionModelConstant = "Question";
 export const strResponseModelConstant = "Response";
+export const strAnswerModelConstant = "Answer";
