83a1a056bfced07fb03d8f95c2541cdc89f3d38e
Dubjay18 <jejeniyi7@gmail.com>
2023-12-13 10:42:28 +0100

    feat auth service: verify signup otp

--- src/services/user/auth.service.ts
+++ src/services/user/auth.service.ts
@@ -1,12 +1,20 @@
+/**
+ * Sends a sign-up OTP (One-Time Password) to the specified email address.
+ * @param body - The request body containing the email address.
+ * @throws BadRequestError - If the email address already exists or an OTP has been sent within the last minute.
+ */
 import { jwtPrivateKey } from "@/config/constants";
 import { JwtHelper } from "@/helpers/jwt.helper";
 import userAuthTokenModel from "@/models/userAuthToken.model";
-import { ISignupOtpRequest } from "../types";
+import {
+  ISignupOtpRequest,
+  ISignupOtpVerifyRequest,
+} from "../types";
 import userModel from "@/models/user.model";
 import BadRequestError from "@/errors/BadRequestError";
 import userVerificationModel from "@/models/userVerification.model";
 import { generateOtp } from "@/utils/general/string.util";
-import { OtpType } from "@/models/types";
+import { OtpType, UserVerification } from "@/models/types";
 
 const jwtHelper = new JwtHelper({
   privateKey: jwtPrivateKey,
@@ -48,4 +56,37 @@ export async function sendSignUpOtp(
   });
   // Save the record
   await newVerification.save();
+  // TODO: send email
+}
+
+export async function verifySignupOtp(
+  body: ISignupOtpVerifyRequest
+): Promise<string> {
+  const { otp } = body;
+  let { email } = body;
+  email = email.toLowerCase();
+  const verification =
+    await userVerificationModel.findOne<UserVerification>({
+      email,
+      otp,
+    });
+  if (!verification) {
+    throw new BadRequestError({
+      message: "Invalid OTP",
+    });
+  } else if (
+    new Date(verification.expiresAt) < new Date()
+  ) {
+    throw new BadRequestError({
+      message: "OTP expired",
+    });
+  }
+  // Generate the JWT.
+  const token = jwtHelper.generateToken({
+    email,
+  });
+
+  // delete used record
+  verification.deleteOne();
+  return token;
 }
